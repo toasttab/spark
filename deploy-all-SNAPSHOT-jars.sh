24a8e144df63b043b85dce3a8efb56dacee539ad
Avanti Patil <apatil@toasttab.com>
2017-05-15 13:37:08 -0400

    works for parent spark jar deploy

--- deploy-all-SNAPSHOT-jars.sh
+++ deploy-all-SNAPSHOT-jars.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#moduleName=$1
+
 scalaVersion="2.11"
 version="2.0.3"
-#for moduleName in `cat spark-module-list.txt`
-#   do
-#      if [ -z $moduleName ] 
-#      then
-#         echo" No module provided"
-#      else
-#         echo "Module name : "$moduleName
-#         mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
-#        if [ $? -ne 0 ]
-#	then
-#	   echo "Failed to deploy module : "$moduleName
-#	   break
-#	fi
-#   fi
 
 mvn deploy:deploy-file -Dfile=./target/spark-parent_2.11-2.0.3-SNAPSHOT-tests.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
-#done;
+
+for moduleName in `cat spark-module-list.txt`
+do
+   if [ -z $moduleName ] ; then
+      echo" No module provided"
+   else
+      echo "Module name : "$moduleName
+      mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
+     if [ $? -ne 0 ] ; then
+        echo "Failed to deploy module : "$moduleName
+        break
+     fi
+   fi
+done
+
