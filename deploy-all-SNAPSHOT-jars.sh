3ab8559994e9b0933576d46609b95af21911fbfb
Avanti Patil <apatil@toasttab.com>
2017-11-16 17:42:24 -0500

    add quotes for param

--- deploy-all-SNAPSHOT-jars.sh
+++ deploy-all-SNAPSHOT-jars.sh
@@ -3,7 +3,12 @@
 scalaVersion="2.11"
 version="2.1.2"
 
-mvn deploy:deploy-file -Dfile=./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
+ls ./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar
+
+mvn deploy:deploy-file -Dfile=./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion="$version"-SNAPSHOT
+
+exit 0
+ls ./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar
 
 for moduleName in `cat spark-module-list.txt`
 do
@@ -11,7 +16,7 @@ do
       echo" No module provided"
    else
       echo "Module name : "$moduleName
-      mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
+      mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion="$version"-SNAPSHOT
      if [ $? -ne 0 ] ; then
         echo "Failed to deploy module : "$moduleName
         break
