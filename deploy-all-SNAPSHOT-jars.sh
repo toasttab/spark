69f0ac3925a97318326022c28e61d68ee9dfe656
Martin Kressirer <mkressirer@toasttab.com>
2018-01-31 10:41:24 -0500

    update versions to 2.3.1

--- deploy-all-SNAPSHOT-jars.sh
+++ deploy-all-SNAPSHOT-jars.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 scalaVersion="2.11"
-version="2.2.2"
+version="2.3.1"
 
-mvn deploy:deploy-file -Dfile=./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
+mvn deploy:deploy-file -Dfile=./target/spark-parent_"$scalaVersion"-"$version"-SNAPSHOT-tests.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion="$version"-SNAPSHOT
+
 
 for moduleName in `cat spark-module-list.txt`
 do
@@ -11,11 +12,10 @@ do
       echo" No module provided"
    else
       echo "Module name : "$moduleName
-      mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion=$version-SNAPSHOT
+      mvn deploy:deploy-file -Dfile=./assembly/target/scala-"$scalaVersion"/jars/"$moduleName"_"$scalaVersion"-"$version"-SNAPSHOT.jar -DrepositoryId=snapshots -Durl=https://artifactory.eng.toasttab.com/artifactory/libs-snapshot-local -DgroupId=org.apache.spark -DartifactId="$moduleName"_"$scalaVersion" -Dversion="$version"-SNAPSHOT
      if [ $? -ne 0 ] ; then
         echo "Failed to deploy module : "$moduleName
         break
      fi
    fi
 done
-
